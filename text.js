ca41352313b85ee6bd8693b51270a7867127056c
hbyx <2248195520@qq.com>
2022-10-18 21:29:18 +0800

    测试文件

---
+++ text.js
@@ -0,0 +1,13 @@
+
+				function big(){
+					document.body.style.fontSize = "25px"
+					document.body.style.color = "red"
+				}
+				function center(){
+					document.body.style.fontSize = "16px"
+					document.body.style.color = "blue"
+				}
+				function samll(){
+					document.body.style.fontSize = "5px"
+					document.body.style.color = "yellow"
+				}
